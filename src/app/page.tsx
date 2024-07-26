403cc2a8f544a67401c11f1c4cbaa2d9e3ee980c
MANIAC <maniac@pop-os.localdomain>
2024-07-26 20:56:20 +0530

    message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
 import Homescreen from "@/components/Homescreen";
-import Portfolioscreen from "@/components/Portfolioscreen";
 export default function Home() {
   return (
     <>
     <Homescreen/>
-    {/* <Portfolioscreen/> */}
     </>
   );
 }
