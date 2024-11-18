f3544e40a1a76c808224e027460e539c6a2e6564
MANIAC <maniac@pop-os.localdomain>
2024-11-18 09:55:34 +0530

    changed cineaste to rhitam's portfolio

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { TracingBeamDemo } from './About'
 const page = () => {
   return (
-    <div className='border border-black'>
+    <div className='border border-black bg-black'>
         <TracingBeamDemo/>
     </div>
   )
